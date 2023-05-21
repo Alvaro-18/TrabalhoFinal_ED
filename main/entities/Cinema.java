f3eb4a44980622e84e6cfbe9cab467dfd795792c
Gerdany Junior <Asa_lindo@hotmail.com>
2023-05-21 20:55:21 -0300

    Cria o método para createSession

--- main/entities/Cinema.java
+++ main/entities/Cinema.java
@@ -13,7 +13,14 @@ public class Cinema {
     }
 
     public void openSession(Integer sessionNumber) {
-        sessions.get(sessionNumber).setOpen(true);
+        if(sessions.get(sessionNumber) != null) {
+            if(!sessions.get(sessionNumber).isOpen()) {
+                sessions.get(sessionNumber).setOpen(true);
+            }
+            else {
+                System.err.println("Sessão já aberta!");
+            }
+         }
     }
 
     public void createSession(String title, Integer minutes, LocalDateTime time, String nationality, String audio,
@@ -21,12 +28,33 @@ public class Cinema {
         for (Session session : sessions) {
             if (Duration.between(time, session.getTime()).toMillis() > 1200000) {
                 System.out.println("Não é possível criar um sessão nesse horário");
+                break;
             } else if (session.getRoomNumber() == roomNumber) {
                 System.out.println("A sala informada já está ocupada, favor escolher uma nova sala");
+                break;
             } else {
                 sessions.add(new Session(title, minutes, time, nationality, audio, threeDimensions, price, roomNumber));
                 System.out.println("A sessão foi registrada com sucesso!");
             }
+        }
+    }
+
+    public void changeSession(Integer index, Session updatedSession) {
+        if (sessions.get(index) != null) {
+            if (!sessions.get(index).isOpen()) {
+                for (Session session : sessions) {
+                    if (!(Duration.between(updatedSession.getTime(), session.getTime()).toMillis() > 1200000)) {
+                        sessions.set(index, updatedSession);
+                    } else {
+                        System.err.println("Não é possível atualizar uma sessão nesse período!");
+                        break;
+                    }
+                }
+            } else {
+                System.err.println("Não é possível alterar uma sessão aberta!");
+            }
+        } else {
+            System.err.print("Sala não encontrada!");
         }
     }
 
